85cba48bb6ad80d007aa5beed03ea06e211f0991
Alexander Scherbachenko <alexander.scherbachenko@dualbootpartners.com>
2020-04-22 20:33:39 +0400

    Policies initialization removed.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,20 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use App\Policies\TaskStatusPolicy;
-use App\Policies\TaskPolicy;
-use App\Policies\LabelPolicy;
-use App\TaskStatus;
-use App\Task;
-use App\Label;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    protected $policies = [
-        Task::class => TaskPolicy::class,
-        Label::class => LabelPolicy::class,
-        TaskStatus::class => TaskStatusPolicy::class,
-    ];
+    protected $policies = [];
 
     public function boot()
     {
